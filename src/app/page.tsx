96d892a96fc551a780444631ac20656cb4976993
Bakkaiah <bakkaiahsf@gmail.com>
2025-07-20 21:53:52 +0100

    Initial commit: connected Next.js to WPGraphQL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
-// src/app/page.tsx
-import { getCourses } from '@/lib/fetch';
+import { Post } from '@/types';
+import { getCourses } from '@/lib/fetch'; // 
 
 export default async function Home() {
   const data = await getCourses();
 
   return (
     <main className="p-8 grid gap-6 md:grid-cols-3">
-      {data?.posts?.nodes?.map((post: any) => (
-        <article
-          key={post.id}
-          className="border rounded-xl p-4 shadow hover:shadow-lg transition"
-        >
+      {data.posts.nodes.map((post: Post) => (
+        <article key={post.id} className="border rounded-xl p-4 shadow hover:shadow-lg transition">
           <h2 className="text-lg font-bold mb-2">{post.title}</h2>
           <div
             className="text-sm text-gray-600"
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+// src/types/index.ts
+
+export type Post = {
+    id: string;
+    title: string;
+    excerpt: string;
+};
